4781290e69416a974d81bcbc5da28743b863d08b
2akhilagd <2akhilagd@gmail.com>
2026-09-18 10:24:41 +0530

    Initial React Project

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+/*function App(){
+  return(
+    <div>
+      <h1>Hello Friends!</h1>
+      <p>chai peelo</p>
+    </div>
+  );
+}
+export default App;*/
+/*import "./App.css"
+function App(){
+  const trainerName="Nirmal";
+  const trainingDay=3;
+  return(
+    <main className="app">
+      <section className="welcome-card">
+        <p className="day-label">React Day {trainingDay}</p>
+        <h1 className="title">College Course Explorer</h1>
+        <p>Trainer:{trainerName}</p>
+        <p>React Learning Project</p>
+        <p>Used React,JSX,components,CSS</p>
+      </section>
+
+    </main>
+    
+  );
+}
+export default App;*/
+
+import "./App.css";
+import Navbar from "./compenents/Navbar";
+function App(){
+  return(
+    <div>
+      <Navbar />
+      <main id="home">
+        <h1>Campus Connect</h1>
+        <p>Welcome to college Events and Clubs</p>
+      </main>
+      
+    </div>
+  );
+}
+export default App;
